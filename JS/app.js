9e130a80a2692cdc05a2ea8fa190631eeb438c31
Ana <143552270+dsgana@users.noreply.github.com>
2025-04-11 21:12:24 -0300

    Create app.js

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+// Usando console 
+console.log('Usando o console');
+console.log('Essas mensagens aparecerão console');
+
+// Mensagem de boas-vindas
+console.log('Bem-vindo ao Connect Gamers!');
+console.log('Explore a plataforma e aproveite as competições!');
+
+// Página carregada 
+console.log('A página inicial foi carregada com sucesso!');
